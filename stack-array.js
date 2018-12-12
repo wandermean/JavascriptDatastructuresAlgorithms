d726cbc11d3f65438ccf09ef3aac2a6837b90ff6
Zhe Chen <cz7265050@163.com>
2018-12-12 23:26:54 +0800

    Initial commit

---
+++ stack-array.js
@@ -0,0 +1,36 @@
+/*
+ * @Author: Zhe Chen
+ * @Date: 2018-12-12 23:09:06
+ * @Last Modified by: Zhe Chen
+ * @Last Modified time: 2018-12-12 23:10:02
+ */
+
+class StackArray {
+  constructor() {
+    this.items = [];
+  }
+  push(element) {
+    this.items.push(element);
+  }
+  pop() {
+    return this.items.pop();
+  }
+  peek() {
+    return this.items[this.items.length - 1];
+  }
+  isEmpty() {
+    return this.items.length === 0;
+  }
+  size() {
+    return this.items.length;
+  }
+  clear() {
+    this.items = [];
+  }
+  toArray() {
+    return this.items;
+  }
+  toString() {
+    return this.items.toString();
+  }
+}
---
+++ stack.js
@@ -0,0 +1,37 @@
+/*
+ * @Author: Zhe Chen 
+ * @Date: 2018-12-12 23:09:14 
+ * @Last Modified by:   Zhe Chen 
+ * @Last Modified time: 2018-12-12 23:09:14 
+ */
+function Stack(){
+  let items = []
+  //push 添加新元素到栈顶
+  this.push = function(value){ 
+    items.push(value)
+  }
+  //pop  移除栈顶的元素并将其返回
+  this.pop = function(){
+    return items.pop()
+  }
+  //peek 返回栈顶的元素，不改变栈
+  this.peak = function(){
+    return items[items.length-1]
+  }
+  //检查栈是否为空
+  this.isEmpty = function(){
+    return items.length == 0
+  }
+  // 返回栈中元素个数
+  this.size = function(){
+    return items.length
+  }
+  // 清空栈中元素
+  this.clear = function(){
+    items = []
+  }
+  // 控制台打印栈中元素
+  this.print = function(){
+    console.log(this.items.toString())
+  }
+}
